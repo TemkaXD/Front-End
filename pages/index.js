d97a9444b004b6b014ff239498d1ed0d637ac537
new name <nest22261002@nhs.edu.mn>
2024-12-20 08:15:38 +0800

    Search names

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,55 @@
-import Image from "next/image";
+import { useRouter } from "next/router";
+
 export default function Home() {
+  const router = useRouter();
   return (
-<div>1</div>
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center py-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl w-full px-4">
+        <button onClick={() =>{router.push("cv")}} className="bg-white shadow-lg rounded-lg p-6">
+          <h2 className="text-xl font-bold mb-4 text-center text-black">CV</h2>
+          <div className="bg-gray-200 w-full h-40 flex items-center justify-center rounded-lg">
+            <img src="/cv.jpeg" alt="cv.js"/>
+          </div>
+        </button>
+
+        
+        <button onClick={() =>{router.push("weather")}} className="bg-white shadow-lg rounded-lg p-6">
+          <h2 className="text-xl font-bold mb-4 text-center text-black">WEATHER</h2>
+          <div className="bg-gray-200 w-full h-40 flex items-center justify-center rounded-lg">
+            <img src="/Weather.jpeg" alt="Weather.js"/>
+          </div>
+        </button>
+
+        
+        <button onClick={() =>{router.push("grid")}} className="bg-white shadow-lg rounded-lg p-6">
+          <h2 className="text-xl font-bold mb-4 text-center text-black">GRID</h2>
+          <div className="bg-gray-200 w-full h-40 flex items-center justify-center rounded-lg">
+            <img src="grid.jpeg" alt="grid"/>
+          </div>
+        </button>
+
+        
+        <button onClick={() =>{router.push("search")}} className="bg-white shadow-lg rounded-lg p-6">
+          <h2 className="text-xl font-bold mb-4 text-center text-black">SEARCH</h2>
+          <div className="bg-gray-200 w-full h-40 flex items-center justify-center rounded-lg">
+            <img src="/search.jpeg" alt="search.js"/>
+          </div>
+        </button>
+
+        
+        <div className="bg-white shadow-lg rounded-lg p-6">
+          <div className="bg-gray-200 w-full h-40 flex items-center justify-center rounded-lg">
+            <p className="text-gray-500">ROUTER</p>
+          </div>
+        </div>
+
+        
+        <div className="bg-white shadow-lg rounded-lg p-6">
+          <div className="bg-gray-200 w-full h-40 flex items-center justify-center rounded-lg">
+            <p className="text-gray-500">Placeholder</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
---
+++ pages/search2.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+const usersdata = [
+    {id: 1, name: "Amartuvshin", email: "GOLD"},
+    {id: 2, name: "Anar", email: "GOLD"},
+    {id: 3, name: "Anar-Erdene", email: "GOLD"},
+    {id: 4, name: "Batmend", email: "UNRANKED"},
+    {id: 5, name: "Tugs-Asralt", email: "UNRANKED"},
+    {id: 6, name: "B.Temuujin", email: "UNRANKED"},
+    {id: 7, name: "SH.Temuujin", email: "UNRANKED"},
+    {id: 8, name: "E.Temuujin", email: "RADIANT"},
+    {id: 9, name: "Temuulen", email: "SILVER"},
+    {id: 10, name: "Tergel", email: "IMMORTAL"},
+    {id: 11, name: "Khanbileg", email: "DIAMOND"},
+    {id: 12, name: "Tselmeg", email: "IRON"},
+    {id: 13, name: "Choi-odser", email: "GOLD"},
+    {id: 14, name: "Shine-Erdene", email: "GOLD"},
+    {id: 15, name: "Enkhjav", email: "UNRANKED"},
+    {id: 16, name: "Enkhtugs", email: "UNRANKED"},
+    {id: 17, name: "Enkhtugs", email: "UNRANKED"},
+    {id: 18, name: "Emily", email: "DIAMOND"},
+    {id: 19, name: "Dalaisuren", email: "UNRANKED"},
+    {id: 20, name: "Ochir-Erdene", email: "UNRANKED"},
+];
+
+export default function Users() {
+    const [users, setUsers] = useState(usersdata);
+    const [search, setSearch] = useState("");
+    const [grid, setGrid] = useState(true);
+
+    const filteredUsers = users.filter((user) =>
+        user.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    return (
+        <div className="min-h-screen bg-black p-8">
+            <h1 className="text-2xl font-bold mb-4 text-center text-white">
+                Хэрэглэгчийн жагсаалт
+            </h1>
+            <div className="mb-6">
+                <input 
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Хэрэглэгчийн нэрээр хайна уу ..."
+                    className="w-full p-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+                />
+            </div>
+
+            <div className={grid ? "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 pt-8" : "space-x-4"}>
+                {filteredUsers.length > 0 ? (
+                    filteredUsers.map((user) => (
+                        <div
+                            key={user.id}
+                            className="bg-white shadow-md rounded-lg p-4 flex justify-between items-center"
+                        >
+                            <div>
+                                <p className="text-lg font-medium text-gray-700">{user.name}</p> 
+                                <p className="text-sm text-black">{user.email}</p>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p className="text-white">Хэрэглэгч олдсонгүй.</p>
+                )}
+            </div>
+
+            <button
+                className="mt-6 text-black bg-white px-4 py-2 rounded"
+                onClick={() => setGrid(!grid)}
+            >
+                {grid ? "TO LIST" : "TO GRID"}
+            </button>
+        </div>
+    );
+}
